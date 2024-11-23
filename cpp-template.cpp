1c9b90e2f749924763ebecc6e0ed598ebccf010e
jstmax! <87650746+jstmaxlol@users.noreply.github.com>
2024-11-23 20:46:30 +0100

    Beauty-fied

--- cpp-template.cpp
+++ cpp-template.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
 #include <string>
-#include <vector>
 // Language: C++
 // (un)Licensed by the Unlicense, for more information, please refer to <https://unlicense.org>
 
 void usage() {
     std::cout << "usage: [--test | -t > Description for the argument]\n"
-              << "       [--help | -h > e.g. Outputs usage and exits.]\n";
+    << "       [--help | -h > e.g. Outputs usage and exits.]\n";
 }
 
 int main(int argc, char* argv[]) {
     if (argc == 1) {
         // If no argument was given
         std::cout << "no arguments were given\n";
-        return 1;
+        return 1; // Optionally returns a non-zero value, which can be used to signal that an error occurred
     }
 
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
 
         if (arg == "--test" || arg == "-t") {
-            // If the `--test` or `-t` argument gets parsed
+            // If the `--test` or `-t` argument gets parsed, this will run
             std::cout << "test parsed\n";
-        } else if (arg == "--help" || arg == "-h") {
-            usage();
+        }
+
+        else if (arg == "--help" || arg == "-h") {
+            // If the `--test` or `-t` argument gets parsed, this will run
+            usage(); // Calls the `usage` function, outputting the usage message
             return 0;
-        } else {
+        }
+
+        else {
             // If the argument parsed isn't in the list above
             std::cout << "unknown option: " << arg << "\n";
             usage();
-            return 1;
+            return 1; // Optionally returns a non-zero value, which can be used to signal that an error occurred
         }
     }
-
     return 0;
 }
 
